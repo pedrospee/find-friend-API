6ce7ed73f8bcd0679e91078d586632c7691360c5
Pedro <pedrosss7770@gmail.com>
2026-09-07 16:09:13 +0200

    Bootstrap Fastify app and HTTP server entrypoint

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { app } from './app.js'
+import { env } from './env/index.js'
+
+app
+  .listen({ port: env.PORT })
+  .then(() => {
+    console.log(`HTTP server running on port ${env.PORT}`)
+  })
